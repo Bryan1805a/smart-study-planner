211dcffdcea9fd41e2a9c6e2af4f826d70ce2e65
Bryan <buungoc1805@gmail.com>
2026-04-12 15:18:53 +0700

    Add: cookie-parser to connect between backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/auth');
 
 // Load env
 dotenv.config();
 
 // Init
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
 // Middleware setup
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
+// Routes
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('The server is running.');
