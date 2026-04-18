aa99e48609fbc63747c1b5b5d8ac42cdb4232517
Bryan <buungoc1805@gmail.com>
2026-04-18 14:52:02 +0700

    feat: add Timer function to UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Timer from './pages/Timer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path='/timer' element={<Timer />} />
     </Routes>
   );
 }
